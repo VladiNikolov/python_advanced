7645fd37d0b346618559f0111c2dba0ae733796c
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-09-14 18:12:49 +0300

    Update 01.reverse_strings.py

--- lists_as_stacks_and_queues_lab/01.reverse_strings.py
+++ lists_as_stacks_and_queues_lab/01.reverse_strings.py
@@ -1,1 +1,12 @@
-a= 10
+input_line = input()
+
+stack = []
+for element in input_line:
+    stack.append(element)
+
+reversed_string = ""
+
+while len(stack) > 0:
+    reversed_string += stack.pop()
+
+print(reversed_string)
