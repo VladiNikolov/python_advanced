209cb090d22d5f52262bf879dd4bd01d63148627
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-09-18 15:57:18 +0300

    Create 01.unique_usernames.py

---
+++ tuples_and_sets_exercise/01.unique_usernames.py
@@ -0,0 +1,9 @@
+input_lines = int(input())
+
+unique_username = set()
+
+for _ in range(input_lines):
+    input_name = input()
+    if input_name not in unique_username:
+        unique_username.add(input_name)
+[print(name) for name in unique_username]
